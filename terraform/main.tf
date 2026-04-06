bdf691e337064cee93fe75f1e951ac4c2181baad
Miles Josephson <jos21004@byui.edu>
2026-04-05 20:32:22 -0600

    fix RDS engine version

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,7 +111,7 @@ resource "aws_security_group" "rds_sg" {
 resource "aws_db_instance" "postgres" {
   identifier        = "jdm-legacy-db"
   engine            = "postgres"
-  engine_version    = "16.3"
+  engine_version    = "16"
   instance_class    = "db.t3.micro"
   allocated_storage = 20
   storage_type      = "gp2"
